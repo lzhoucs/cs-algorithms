f839f76ca028f162080433a5f5e19301ae1130b2
Liang Zhou <lzhoucs@gmail.com>
2015-03-23 22:06:50 -0500

    Add quicksort javascript implementation.
    lzhoucs|G580|Win8|CMD

---
+++ src/sorting/quickSort.js
@@ -0,0 +1,39 @@
+/* 
+	in-place, plain (no javascript API is used), so code is portable to any languages.
+	pivotFunc : function to find a pivot, optional
+*/
+function quickSort(arry, pivotFunc) {
+
+	function _swap(indx1, indx2) {
+		var tmp = arry[indx1];
+		arry[indx1] = arry[indx2];
+		arry[indx2] = tmp;
+	}
+	/*
+		i - first bigger element index
+	*/
+	function _quickSort(l, r) {
+		var length = r - l,
+		i = j = l + 1;
+			
+		if(length > 1) {
+
+			if(pivotFunc) 
+				_swap(pivotFunc(arry, l, r), l);
+
+			for(; j < r, j++) {
+				if(arry[j] < arry[l]) {
+					_swap(i, j);
+					i++;
+				} // else do nothing
+			}
+
+			_swap(l, i - 1); // put pivot to its right position
+			
+			_quickSort(l, i - 2);
+			_quickSort(i, r);
+
+		} // else do nothing.
+	}
+	_quickSort(0, arry.length);
+}
