de2c9b7b074e422d1bd99d25435551197ac46b89
lzhoucs <lzhoucs@gmail.com>
2015-04-07 07:53:04 -0500

    Still a work in progress.
    lzhoucs|G580|Mint17|WebStorm10

--- src/application/toBeRenamed.js
+++ src/application/toBeRenamed.js
@@ -1,9 +1,17 @@
-var     crTool  = require('../tools/courseraTool');
-
+var     crTool  = require('../tools/courseraTool'),
+        alg     = require('../graph/DFS');
+        aldg    = require('../graph/adjacencyListDirectedGraph')
 
 crTool.get2DNumberArray('http://spark-public.s3.amazonaws.com/algo1/programming_prob/SCC.txt', function(data) {
+    var graph = new aldg.AdjacencyListDirectedGraph(data);
+    var vertexList = graph.getVertexList();
+    for(var i = vertexList.length - 1; i >=0; i--) {
+        if(!vertexList[i].isExplored()) {
+            //TODO To Be Continued
+            alg.DFS( )
+        }
 
-    console.log("Data length : " + data.length);
+    }
 
 
 });
--- src/graph/DFS.js
+++ src/graph/DFS.js
@@ -1,11 +1,16 @@
-function DFS(graph, vertex){
+exports.DFS = function (graph, vertex, beginCallback, endCallback){
 
     var _DFS = function(v) {
+        if(beginCallback)
+            beginCallback(v);
         v.setExplored(true);
-        graph.getAdjaccencyList(v).forEach(function (el) {
+        graph.getAdjacencyList(v).forEach(function (el) {
             if(!el.isExplored())
                 _DFS(el);
         });
+        if(endCallback)
+            endCallback(v);
     }
+
     _DFS(vertex)
 }
