b4975d8964bbeb645255966d0808b2553d196d76
Liang Zhou <lzhoucs@gmail.com>
2015-04-10 17:05:40 -0500

    Minor change.
    liangz|E5540|Win7|WebStorm9EVL

--- src/application/toBeRenamed.js
+++ src/application/toBeRenamed.js
@@ -1,14 +1,18 @@
 var     crTool  = require('../tools/courseraTool'),
-        alg     = require('../graph/DFS');
-        aldg    = require('../graph/adjacencyListDirectedGraph')
+        alg     = require('../graph/DFS'),
+        aldg    = require('../graph/adjacencyListDirectedGraph');
 
 crTool.get2DNumberArray('http://spark-public.s3.amazonaws.com/algo1/programming_prob/SCC.txt', function(data) {
     var graph = new aldg.AdjacencyListDirectedGraph(data);
-    var vertexList = graph.getVertexList();
-    for(var i = vertexList.length - 1; i >=0; i--) {
-        if(!vertexList[i].isExplored()) {
+    var verticesList = graph.getVerticesList();
+    for(var i = verticesList.length - 1; i >=0; i--) {
+        if(!verticesList[i].isExplored()) {
             //TODO To Be Continued
-            alg.DFS( )
+            alg.DFS( graph, verticesList[i], function (vertex) {
+
+            }, function (vertex) {
+
+            })
         }
 
     }
