5d42b7c981a70b6eb7388279633451088479b9ac
Liang Zhou <lzhoucs@gmail.com>
2015-04-22 17:05:52 -0500

    Refactor SCCByDFS : Extract the core logic into a function so it is more testable. Add test cases for it.
    liangz|E5540|Win7|WebStorm9EVL

--- src/application/SCCByDFS.js
+++ src/application/SCCByDFS.js
@@ -6,7 +6,7 @@ var     crTool  = require('../tools/courseraTool'),
         T = 1,
         S = null,
 
-        SCCByDFS = function (graph, vertexIndicesList, beginCallback, endCallback) {
+        DFSLoop = function (graph, vertexIndicesList, beginCallback, endCallback) {
             for(var i = vertexIndicesList.length - 1; i >=0; i--) {
                 var vertex = graph.getVertexByIndx(vertexIndicesList[i]);
 
@@ -16,56 +16,57 @@ var     crTool  = require('../tools/courseraTool'),
                     alg.DFS( graph, vertexIndicesList[i], beginCallback, endCallback)
                 }
             }
+        },
+
+        /**
+         * compute strongly connected components (SCCs).
+         */
+         SCCByDFS = function(data) {
+
+            console.log("1. Got data.");
+
+            var graph = new aldg.AdjacencyListDirectedGraph(data),
+                reversedGraph = new aldg.AdjacencyListDirectedGraph(data.map(function (el) {
+
+                    return util.arrySwap(el, 0, 1);
+                })),
+
+                finishingTimes = [],
+                leaders = [],
+                leaderBoard = [];
+
+            // first pass
+            DFSLoop(reversedGraph, reversedGraph.getVertexIndicesList(), null, function (indx) {
+                finishingTimes[indx] = T++;
+            });
+            console.log("2. First pass done.");
+
+            graph.reorder(finishingTimes);
+            // second pass
+            DFSLoop(graph, graph.getVertexIndicesList(), function (indx) {
+                leaders[indx] = S;
+            }, null);
+
+            console.log("3. Second pass done.");
+
+            leaders.forEach(function (indx) {
+                var score = leaderBoard[indx];
+                if(score)
+                    score++;
+                else
+                    score = 1;
+                leaderBoard[indx] = score;
+
+            })
+
+            //descending order
+            leaderBoard.sort(function (a, b) {
+                return b - a;
+            });
+            console.log(leaderBoard);
+
         }
 
-
-/**
- * compute strongly connected components (SCCs).
- */
-crTool.get2DNumberArray('http://spark-public.s3.amazonaws.com/algo1/programming_prob/SCC.txt', function(data) {
-
-    console.log("1. Got data.");
-
-    var graph = new aldg.AdjacencyListDirectedGraph(data),
-        reversedGraph = new aldg.AdjacencyListDirectedGraph(data.map(function (el) {
-
-            return util.arrySwap(el, 0, 1);
-        })),
-
-        finishingTimes = [],
-        leaders = [],
-        leaderBoard = [];
-
-    // first pass
-    SCCByDFS(reversedGraph, reversedGraph.getVertexIndicesList(), null, function (indx) {
-        finishingTimes[indx] = T++;
-    });
-    console.log("2. First pass done.");
-
-    graph.reorder(finishingTimes);
-    // second pass
-    SCCByDFS(graph, graph.getVertexIndicesList(), function (indx) {
-        leaders[indx] = S;
-    }, null);
-
-    console.log("3. Second pass done.");
-
-    leaders.forEach(function (indx) {
-        var score = leaderBoard[indx];
-        if(score)
-            score++;
-        else
-            score = 1;
-        leaderBoard[indx] = score;
-
-    })
-
-    //descending order
-    leaderBoard.sort(function (a, b) {
-        return b - a;
-    });
-    console.log(leaderBoard);
-
-});
+crTool.get2DNumberArray('http://spark-public.s3.amazonaws.com/algo1/programming_prob/SCC.txt', SCCByDFS);
 
 
