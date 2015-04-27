5f937228d11d0a508c2bd726fc9ea4f5847a6707
lzhoucs <lzhoucs@gmail.com>
2015-04-26 23:39:04 -0500

    Keep it as a one-stop note to keep track of javascript questions.
    lzhoucs|G580|Mint17|WebStorm10

---
+++ test/misc/howStuffWorksTest.js
@@ -0,0 +1,18 @@
+var     assert = require("assert");
+
+
+describe('Array', function(){
+
+    describe('#map() with null/undefined values', function(){
+        it('should return undefined when no return(condition doesn\' meet)', function(){
+
+            assert.deepEqual([101,undefined, undefined, "2100"], [1,undefined,null, "2"].map(function (el) {
+                if(el)
+                    return el + 100;
+            }));
+
+        })
+    })
+
+
+})
